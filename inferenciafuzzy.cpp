4c40d4d415fcd658ad2f94f869ff631835e1e579
SebasSandon <sebastian_sandon@hotmail.com>
2017-07-16 01:22:10 -0400

    Archivo inferenciafuzzy.cpp restaurado de la versión anterior

--- inferenciafuzzy.cpp
+++ inferenciafuzzy.cpp
@@ -44,35 +44,168 @@ void InferenciaFuzzy::InferenciaMamdani(){
         ConjuntoDifuso Riesgos[4];
 
         //Regla 1
-        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)) >= 0.4){
+        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)) >= 0.1){
             Riesgos[0].SetValor(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)));
-            Riesgos[0].PreCalculoDivisor(80,100);
-            Riesgos[0].PreCalculoDividendo(80,100);
+            Riesgos[0].PreCalculoDivisor(90,100);
+            Riesgos[0].PreCalculoDividendo(90,100);
         }
 
         //Regla 2
-        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)) >= 0.4){
+        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)) >= 0.1){
             Riesgos[1].SetValor(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)));
-            Riesgos[1].PreCalculoDivisor(80,100);
-            Riesgos[1].PreCalculoDividendo(80,100);
+            Riesgos[1].PreCalculoDivisor(90,100);
+            Riesgos[1].PreCalculoDividendo(90,100);
         }else{
             Riesgos[1].SetValor(0);
-            Riesgos[1].PreCalculoDivisor(80,100);
-            Riesgos[1].PreCalculoDividendo(80,100);
+            Riesgos[1].PreCalculoDivisor(90,100);
+            Riesgos[1].PreCalculoDividendo(90,100);
         }
 
         //Regla 3
-        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Algo_retraso,Algo_importante)) >= 0.4){
+        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Algo_retraso,Algo_importante)) >= 0.1){
             Riesgos[2].SetValor(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Algo_retraso,Algo_importante)));
-            Riesgos[2].PreCalculoDivisor(50,90);
-            Riesgos[2].PreCalculoDividendo(50,90);
+            Riesgos[2].PreCalculoDivisor(65,80);
+            Riesgos[2].PreCalculoDividendo(65,80);
         }
 
         //Regla 4
-        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)) >= 0.4){
-            Riesgos[3].SetValor(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)));
-            Riesgos[3].PreCalculoDivisor(50,90);
-            Riesgos[3].PreCalculoDividendo(50,90);
+        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)) >= 0.1){
+            Riesgos[3].SetValor(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)));
+            Riesgos[3].PreCalculoDivisor(65,80);
+            Riesgos[3].PreCalculoDividendo(65,80);
+        }
+
+        //Regla 5
+        if(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)) >= 0.1){
+            Riesgos[4].SetValor(operador.FuzzyAND(Nada_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)));
+            Riesgos[4].PreCalculoDivisor(65,80);
+            Riesgos[4].PreCalculoDividendo(65,80);
+        }
+
+        //Regla 6
+        if(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)) >= 0.1){
+            Riesgos[5].SetValor(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)));
+            Riesgos[5].PreCalculoDivisor(90,100);
+            Riesgos[5].PreCalculoDividendo(90,100);
+        }
+
+        //Regla 7
+        if(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)) >= 0.1){
+            Riesgos[6].SetValor(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)));
+            Riesgos[6].PreCalculoDivisor(90,100);
+            Riesgos[6].PreCalculoDividendo(90,100);
+        }
+
+        //Regla 8
+        if(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Algo_retraso,Algo_importante)) >= 0.1){
+            Riesgos[7].SetValor(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Algo_retraso,Algo_importante)));
+            Riesgos[7].PreCalculoDivisor(65,80);
+            Riesgos[7].PreCalculoDividendo(65,80);
+        }
+
+        //Regla 9
+        if(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)) >= 0.1){
+            Riesgos[8].SetValor(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)));
+            Riesgos[8].PreCalculoDivisor(40,60);
+            Riesgos[8].PreCalculoDividendo(40,60);
+        }
+
+        //Regla 10
+        if(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)) >= 0.1){
+            Riesgos[9].SetValor(operador.FuzzyAND(Pobre_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)));
+            Riesgos[9].PreCalculoDivisor(65,80);
+            Riesgos[9].PreCalculoDividendo(65,80);
+        }
+
+        //Regla 11
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)) >= 0.1){
+            Riesgos[10].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)));
+            Riesgos[10].PreCalculoDivisor(90,100);
+            Riesgos[10].PreCalculoDividendo(90,100);
+        }
+
+        //Regla 12
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Mucho_retraso,Algo_importante)) >= 0.1){
+            Riesgos[11].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Mucho_retraso,Algo_importante)));
+            Riesgos[11].PreCalculoDivisor(65,80);
+            Riesgos[11].PreCalculoDividendo(65,80);
+        }
+
+        //Regla 13
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Mucho_retraso,Poco_importante)) >= 0.1){
+            Riesgos[12].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Mucho_retraso,Poco_importante)));
+            Riesgos[12].PreCalculoDivisor(40,60);
+            Riesgos[12].PreCalculoDividendo(40,60);
+        }
+
+        //Regla 14
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)) >= 0.1){
+            Riesgos[13].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)));
+            Riesgos[13].PreCalculoDivisor(65,80);
+            Riesgos[13].PreCalculoDividendo(65,80);
+        }
+
+        //Regla 15
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Algo_retraso,Algo_importante)) >= 0.1){
+            Riesgos[14].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Algo_retraso,Algo_importante)));
+            Riesgos[14].PreCalculoDivisor(40,60);
+            Riesgos[14].PreCalculoDividendo(40,60);
+        }
+
+        //Regla 16
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Algo_retraso,Poco_importante)) >= 0.1){
+            Riesgos[15].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Algo_retraso,Poco_importante)));
+            Riesgos[15].PreCalculoDivisor(20,35);
+            Riesgos[15].PreCalculoDividendo(20,35);
+        }
+
+        //Regla 17
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)) >= 0.1){
+            Riesgos[16].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)));
+            Riesgos[16].PreCalculoDivisor(20,35);
+            Riesgos[16].PreCalculoDividendo(20,35);
+        }
+
+        //Regla 18
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)) >= 0.1){
+            Riesgos[17].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)));
+            Riesgos[17].PreCalculoDivisor(20,35);
+            Riesgos[17].PreCalculoDividendo(20,35);
+        }
+
+        //Regla 19
+        if(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Poco_retraso,Poco_importante)) >= 0.1){
+            Riesgos[18].SetValor(operador.FuzzyAND(AlgoBuena_documentacion,operador.FuzzyAND(Poco_retraso,Poco_importante)));
+            Riesgos[18].PreCalculoDivisor(0,10);
+            Riesgos[18].PreCalculoDividendo(0,10);
+        }
+
+        //Regla 20
+        if(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)) >= 0.1){
+            Riesgos[19].SetValor(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Mucho_retraso,Muy_importante)));
+            Riesgos[19].PreCalculoDivisor(65,80);
+            Riesgos[19].PreCalculoDividendo(65,80);
+        }
+
+        //Regla 21
+        if(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)) >= 0.1){
+            Riesgos[20].SetValor(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Algo_retraso,Muy_importante)));
+            Riesgos[20].PreCalculoDivisor(40,60);
+            Riesgos[20].PreCalculoDividendo(40,60);
+        }
+
+        //Regla 22
+        if(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)) >= 0.1){
+            Riesgos[21].SetValor(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Poco_retraso,Muy_importante)));
+            Riesgos[21].PreCalculoDivisor(0,10);
+            Riesgos[21].PreCalculoDividendo(0,10);
+        }
+
+        //Regla 23
+        if(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)) >= 0.1){
+            Riesgos[22].SetValor(operador.FuzzyAND(Buena_documentacion,operador.FuzzyAND(Poco_retraso,Algo_importante)));
+            Riesgos[22].PreCalculoDivisor(0,10);
+            Riesgos[22].PreCalculoDividendo(0,10);
         }
 
         //DESFUSIFICACION
